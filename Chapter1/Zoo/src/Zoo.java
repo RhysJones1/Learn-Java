316e3c0d23eee36044eb0c187831521544e6ff4c
RhysJones1 <91537105+RhysJones1@users.noreply.github.com>
2022-01-28 12:34:15 +0000

    Add files via upload

---
+++ Chapter1/Zoo/src/Zoo.java
@@ -0,0 +1,10 @@
+public class Zoo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I have compiled and run my code through the command line!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+
+    }
+}
