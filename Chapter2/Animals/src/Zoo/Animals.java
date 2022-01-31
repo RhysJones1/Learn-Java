268fcdad6a0e02059abdd344dc4dc67895bc7c8e
RhysJones1 <91537105+RhysJones1@users.noreply.github.com>
2022-01-31 16:54:11 +0000

    Add files via upload

---
+++ Chapter2/Animals/src/Zoo/Animals.java
@@ -0,0 +1,13 @@
+package Zoo;
+
+public class Animals {
+    String animal;
+
+    public static void main(String[] args){
+        Test.main(args);
+        Test1.main(args);
+        System.out.println("Hey I think i've learnt something!");
+
+
+    }
+}
---
+++ Chapter2/Animals/src/Zoo/Test.java
@@ -0,0 +1,11 @@
+package Zoo;
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Testing whether i've learnt something! ");
+        System.out.println("If you are seeing these print in the terminal, good job!");
+
+
+
+    }
+
+}
