8dd071ad1f21691571951461d55c49d767f95400
Rhys Jones <rhys.jones510@gmail.com>
2022-03-01 08:52:25 +0000

    Condensing Folders
    Condensing some folders

---
+++ Java_SE11_Course/Chapter2/Animals/src/Zoo/Test1.java
@@ -0,0 +1,10 @@
+package Zoo;
+
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("Let's see if i can import from a different package");
+
+        }
+    }
+
+
